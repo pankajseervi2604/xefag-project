387b3d8c22583fa2a51364bb57b352494cd34713
Pankaj Seervi A <pankajseervi2604@gmail.com>
2025-02-09 16:56:51 +0530

    finished login frontend design

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,95 +5,195 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 400,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                center: Alignment.topCenter,
-                radius: 0.8,
-                colors: [
-                  Color.fromRGBO(76, 130, 168, 1),
-                  Color.fromRGBO(110, 156, 186, 1),
-                  Color.fromRGBO(147, 182, 207, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  center: Alignment.topCenter,
+                  radius: 0.8,
+                  colors: [
+                    Color.fromRGBO(76, 130, 168, 1),
+                    Color.fromRGBO(110, 156, 186, 1),
+                    Color.fromRGBO(147, 182, 207, 1),
+                  ],
+                ),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  // App Logo
+                  Image.asset(
+                    "assets/images/AppLogos/AppLogo.png",
+                    height: 250,
+                    width: 250,
+                  ),
+                  // App Title
+                  Text(
+                    "Xefag",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 45,
+                      letterSpacing: 3,
+                      color: Colors.white,
+                    ),
+                  ),
                 ],
               ),
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(50),
-              ),
             ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                // App Logo
-                Image.asset(
-                  "assets/images/AppLogos/AppLogo.png",
-                  height: 250,
-                  width: 250,
-                ),
-                // App Title
-                Text(
-                  "Xefag",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 45,
-                    letterSpacing: 3,
-                    color: Colors.white,
+            // Login info
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 15,
                   ),
-                ),
-              ],
-            ),
-          ),
-          // Login info
-          Padding(
-            padding: const EdgeInsets.all(32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  "Welcome Back!",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 30,
+                  Text(
+                    "Welcome Back!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 30,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "To get started, sign in to your account.",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
+                  const SizedBox(
+                    height: 10,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
+                  Text(
+                    "To get started, sign in to your account.",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
 
-                // User Email
-                
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        width: 2,
+                  // User Email
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      hintText: "Enter your Email",
+                      // this will automatically change when user clicks on textfield
+                      // Note : if you are using focusedBorder then border widget is not required
+                      // but the border will not be visible so using enabledBorder , by defualt every textfield is enabled border
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(110, 156, 186, 1),
+                          width: 3,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 1,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // User Password
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.password),
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.remove_red_eye),
+                      ),
+                      hintText: "Enter your Password",
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(110, 156, 186, 1),
+                          width: 3,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Forget Password",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          decorationThickness: 2,
+                          decorationColor: Colors.black,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(110, 156, 186, 1),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Dont't have account?"),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            color: Color.fromRGBO(110, 156, 186, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
